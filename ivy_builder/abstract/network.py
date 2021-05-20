9d2aaa048c34eae0f7e88d9dd6b2b2b8f7b89f66
djl11 <daniel.lenton11@imperial.ac.uk>
2021-05-20 14:42:32 +0200

    added test for ivy trainer using native network classes.

--- ivy_builder/abstract/network.py
+++ ivy_builder/abstract/network.py
@@ -8,11 +8,12 @@ from ivy_builder.specs.network_spec import NetworkSpec
 class Network(ivy.Module):
 
     def __init__(self,
-                 network_spec: NetworkSpec) -> None:
+                 network_spec: NetworkSpec,
+                 v=None) -> None:
         """
         base class for any networks
         """
-        super(Network, self).__init__()
+        super(Network, self).__init__(v=v)
         self._spec = network_spec
 
     # Getters #
