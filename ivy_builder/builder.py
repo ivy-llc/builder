30f505e8b2dfab6e703e019d95304417223b6078
djl11 <daniel.lenton11@imperial.ac.uk>
2021-08-07 14:28:31 +0200

    added option to load classes dynamically from arguments or specifictions into builder methods.

--- ivy_builder/builder.py
+++ ivy_builder/builder.py
@@ -1,7 +1,10 @@
 # global
 import json
+import importlib
 
 # local
+import ivy
+
 from ivy_builder.specs import DataLoaderSpec
 from ivy_builder.specs import DatasetDirs
 from ivy_builder.specs.dataset_spec import DatasetSpec
@@ -20,6 +23,15 @@ __properties_to_ignore = ['activity_regularizer', 'dtype', 'dynamic', 'inbound_n
                           'should_checkpoint', 'should_save_summary', 'worker_devices', 'graph', 'ndim', 'op',
                           'shape', 'value_index', 'aggregation', 'constraint', 'create', 'device', 'handle',
                           'initial_value', 'initializer', 'synchronization']
+
+
+def _import_arg_specified_class_if_present(args_or_spec, class_str):
+    if class_str in args_or_spec:
+        mod_str = '.'.join(args_or_spec[class_str].split('.')[:-1])
+        class_str = args_or_spec[class_str].split('.')[-1]
+        loaded_class = getattr(importlib.import_module(mod_str), class_str)
+        return loaded_class
+    return
 
 
 def parse_json_to_dict(json_filepath):
@@ -68,6 +80,10 @@ def build_dataset_dirs(dataset_dirs_args=None,
     if dataset_dirs_args is None:
         dataset_dirs_args = dict()
 
+    # override dataset_dirs_class if specified in dataset_dirs_args
+    dataset_dirs_class = ivy.default(
+        _import_arg_specified_class_if_present(dataset_dirs_args, 'dataset_dirs_class'), dataset_dirs_class)
+
     # return dataset directories specification
     # noinspection PyArgumentList
     return dataset_dirs_class(**dataset_dirs_args)
@@ -90,6 +106,10 @@ def build_dataset_spec(dataset_dirs_args=None,
         dataset_spec_args = dict()
     dataset_spec_args = {**dataset_spec_args, **{'dirs': dataset_dirs}}
 
+    # override dataset_spec_class if specified in dataset_spec_args
+    dataset_spec_class = ivy.default(
+        _import_arg_specified_class_if_present(dataset_spec_args, 'dataset_spec_class'), dataset_spec_class)
+
     # return dataset specification
     return dataset_spec_class(**dataset_spec_args)
 
@@ -104,6 +124,10 @@ def build_network_specification(network_spec_args=None,
     if network_spec_args is None:
         network_spec_args = dict()
     network_spec_args = {**network_spec_args}
+
+    # override network_spec_class if specified in network_spec_args
+    network_spec_class = ivy.default(
+        _import_arg_specified_class_if_present(network_spec_args, 'network_spec_class'), network_spec_class)
 
     # return network
     # noinspection PyArgumentList
@@ -120,6 +144,10 @@ def build_network(network_class,
     # build network specification
     network_spec = build_network_specification(network_spec_args,
                                                network_spec_class)
+
+    # override network_class if specified in network_spec
+    network_class = ivy.default(
+        _import_arg_specified_class_if_present(network_spec, 'network_class'), network_class)
 
     # network
     return network_class(network_spec)
@@ -154,6 +182,10 @@ def build_data_loader_spec(network_class,
         data_loader_spec_args = dict()
     data_loader_spec_args = {**data_loader_spec_args, **{'dataset_spec': dataset_spec, 'network': network}}
 
+    # override data_loader_spec_class if specified in data_loader_spec_args
+    data_loader_spec_class = ivy.default(
+        _import_arg_specified_class_if_present(data_loader_spec_args, 'data_loader_spec_class'), data_loader_spec_class)
+
     # return data loader
     return data_loader_spec_class(**data_loader_spec_args)
 
@@ -182,6 +214,15 @@ def build_data_loader(data_loader_class,
                                               data_loader_spec_class,
                                               network_spec_args,
                                               network_spec_class)
+
+    if 'data_loader_class' in data_loader_spec:
+        mod_str = '.'.join(data_loader_spec.data_loader_class.split('.')[:-1])
+        class_str = data_loader_spec.data_loader_class.split('.')[-1]
+        data_loader_class = getattr(importlib.import_module(mod_str), class_str)
+
+    # override data_loader_class if specified in data_loader_spec
+    data_loader_class = ivy.default(
+        _import_arg_specified_class_if_present(data_loader_spec, 'data_loader_class'), data_loader_class)
 
     # return data loader
     return data_loader_class(data_loader_spec)
@@ -221,6 +262,10 @@ def build_trainer_spec(data_loader_class,
     trainer_spec_args = {**trainer_spec_args, **{'data_loader': data_loader,
                                                  'network': data_loader.spec.network}}
 
+    # override trainer_spec_class if specified in trainer_spec_args
+    trainer_spec_class = ivy.default(
+        _import_arg_specified_class_if_present(trainer_spec_args, 'trainer_spec_class'), trainer_spec_class)
+
     # return trainer specification
     return trainer_spec_class(**trainer_spec_args)
 
@@ -255,6 +300,10 @@ def build_trainer(data_loader_class,
                                       network_spec_class,
                                       trainer_spec_args,
                                       trainer_spec_class)
+
+    # override trainer_class if specified in trainer_spec
+    trainer_class = ivy.default(
+        _import_arg_specified_class_if_present(trainer_spec, 'trainer_class'), trainer_class)
 
     # return trainer
     return trainer_class(trainer_spec)
@@ -292,6 +341,11 @@ def build_tuner_spec(data_loader_class,
                             trainer_spec_args,
                             trainer_spec_class)
 
+    # override tuner_spec_class if specified in tuner_spec_args
+    tuner_spec_class = ivy.default(
+        _import_arg_specified_class_if_present(tuner_spec_args, 'tuner_spec_class'), tuner_spec_class)
+
+    # return tuner specification
     return tuner_spec_class(trainer,
                             **tuner_spec_args)
 
@@ -315,6 +369,12 @@ def build_tuner(data_loader_class,
     """
     build tuner
     """
+
+    # override tuner_class if specified in tuner_spec_args
+    tuner_class = ivy.default(
+        _import_arg_specified_class_if_present(tuner_spec_args, 'tuner_class'), tuner_class)
+
+    # return tuner
     return tuner_class(data_loader_class,
                        network_class,
                        trainer_class,
