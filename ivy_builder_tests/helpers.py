b365fb4beea0cd965c5dafc65089dfd3e42556f0
djl11 <daniel.lenton11@imperial.ac.uk>
2021-08-30 16:11:15 +0200

    removed test_sequential_scheduler_with_dynamic_source_code_edit unit test.

--- ivy_builder_tests/helpers.py
+++ ivy_builder_tests/helpers.py
@@ -3,8 +3,14 @@ import os
 import shutil
 
 
-def remove_dirs(base_dir=''):
+def remove_dirs(base_dir=None):
+    if base_dir is None:
+        base_dir = os.getcwd()
     shutil.rmtree(os.path.join(base_dir, 'log'), ignore_errors=True)
+    shutil.rmtree(os.path.join(base_dir, 'chkpt'), ignore_errors=True)
+    log_fpath = os.path.join(base_dir, 'log.log')
+    if os.path.isfile(log_fpath):
+        os.remove(log_fpath)
     serialized_model_path = os.path.join(base_dir, 'serialized_model')
     if os.path.isdir(serialized_model_path):
         shutil.rmtree(serialized_model_path, ignore_errors=True)
--- ivy_builder_tests/test_scheduler.py
+++ ivy_builder_tests/test_scheduler.py
@@ -1,14 +1,12 @@
 # global
 import os
-import io
 import time
 import json
 import threading
-from contextlib import redirect_stdout
 
 # local
-from ivy_builder.scheduler import SequentialScheduler
 from ivy_builder_tests import helpers
+from ivy_builder.scheduler import SequentialScheduler
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -66,47 +64,6 @@ def test_sequential_scheduler_with_dynamic_schedule_file_edit(dev_str, call):
 
     with open(schedule_filepath, 'w') as file:
         json.dump(original_schedule_dict, file, indent=2, separators=(',', ': '))
-
-    helpers.remove_dirs()
-    assert success[0]
-
-
-def test_sequential_scheduler_with_dynamic_source_code_edit(dev_str, call):
-    schedule_filepath = os.path.join(THIS_DIR, 'schedule.json')
-    scheduler = SequentialScheduler(schedule_filepath)
-    helpers.remove_dirs()
-
-    src_filepath = os.path.relpath(os.path.join(THIS_DIR, '../demos/full_example.py'))
-    with open(src_filepath, 'r') as file:
-        original_src_lines = file.readlines()
-    msg_to_add = '"Finished complete example!"'
-    new_src_lines = original_src_lines[0:287] + ['    print(' + msg_to_add + ')\n'] + original_src_lines[287:]
-
-    success = [False]
-
-    def modify_src_code_file():
-        time.sleep(0.1)
-        with open(src_filepath, 'w') as src_file:
-            src_file.writelines(new_src_lines)
-
-    def run_scheduler(success_list):
-        f = io.StringIO()
-        with redirect_stdout(f):
-            scheduler.run()
-        if msg_to_add[1:-1] in f.getvalue():
-            success_list[0] = True
-        else:
-            raise Exception('Expected ' + msg_to_add + ' printed in stdout, but it did not print.')
-
-    schedule_thread = threading.Thread(target=run_scheduler, args=(success,))
-    file_write_thread = threading.Thread(target=modify_src_code_file)
-    schedule_thread.start()
-    file_write_thread.start()
-    schedule_thread.join()
-    file_write_thread.join()
-
-    with open(src_filepath, 'w') as file:
-        file.writelines(original_src_lines)
 
     helpers.remove_dirs()
     assert success[0]
