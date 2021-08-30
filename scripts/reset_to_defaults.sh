fecc348830c92221b014f93442f234ddc6f4aeac
djl11 <daniel.lenton11@imperial.ac.uk>
2021-08-30 18:08:10 +0200

    added bash script reset_to_defaults.sh which is saved to the bin directory, used for resetting json specification files to their default values.

---
+++ scripts/reset_to_defaults.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Functions #
+# ----------#
+
+# remove existing non-default json files
+remove_existing_non_default_files() {
+  for json_fname in "$1"/*.json; do
+    if [ -f "$json_fname" ]; then
+      rm "$json_fname"
+    fi
+  done
+}
+
+# create json files for specification loading from their default files, replacing BASE_DIRs with the mount directories
+create_json_files() {
+  for json_fname in "$1"/*.json.defaults; do
+    if [ -f "$json_fname" ]; then
+      # shellcheck disable=SC2001
+      new_json_fname="$(echo "${json_fname}" |sed -e "s|.defaults||g")";
+      cp "$json_fname" "$new_json_fname"
+      sed -i "s|BASE_DATASET_DIR|$2|g" "$new_json_fname"
+      sed -i "s|BASE_LOG_DIR|$3|g" "$new_json_fname"
+    fi
+  done
+}
+
+# recursively create json files in all sub-directories
+recursive_update() {
+  # apply this method recursively to all sub-directories
+  for file in "$1"/*; do
+    if [ -d "${file}" ] ; then
+      recursive_update "${file}" "$2" "$3"
+    fi
+  done
+  # remove existing json files and copy new ones for current directory
+  remove_existing_non_default_files "$1"
+  create_json_files "$1" "$2" "$3"
+}
+
+# end
+end () {
+  echo >&2 "$@"
+  exit 1
+}
+
+# Run #
+# ----#
+
+# initialise option flag with a false value
+OPT_R='false'
+
+# process all options supplied on the command line
+while getopts ':r' 'OPTKEY'; do
+    case ${OPTKEY} in
+        'r')
+            # Update the value of the option x flag we defined above
+            OPT_R='true'
+            ;;
+        '?')
+            echo "INVALID OPTION -- ${OPTARG}" >&2
+            exit 1
+            ;;
+        ':')
+            echo "MISSING ARGUMENT for option -- ${OPTARG}" >&2
+            exit 1
+            ;;
+        *)
+            echo "UNIMPLEMENTED OPTION -- ${OPTKEY}" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# remove all options processed by getopts.
+shift $(( OPTIND - 1 ))
+[[ "${1}" == "--" ]] && shift
+
+# verify dataset and log mount directories are specified
+if [ $# -eq 0 ]
+  then
+    end "dataset and log mount directories must be specified"
+fi
+
+# change to a sub-directory if specified
+if [ $# -eq 3 ]
+  then
+    cd "$3" || exit
+fi
+
+remove_existing_non_default_files "$PWD"
+create_json_files "$PWD" "$1" "$2"
+
+# call the same script in all sub-directories if -r is specified
+if ${OPT_R}; then
+  recursive_update "$PWD" "$1" "$2"
+fi
