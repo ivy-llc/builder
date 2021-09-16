f893b2d13ad91a2b3c4896268adc855a6b75bb6b
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-16 19:36:41 +0200

    bug fix for method locals_to_kwargs.

--- ivy_builder/specs/spec.py
+++ ivy_builder/specs/spec.py
@@ -10,7 +10,12 @@ def locals_to_kwargs(locals_in):
     for k, v in locals_in.items():
         if inspect.isclass(v) or k[0:2] == '__' or k == 'self':
             del locals_cp[k]
-    return {**locals_cp, **(locals_cp['kwargs'] if 'kwargs' in locals_cp else {})}
+    if 'kwargs' in locals_cp:
+        kwargs_dict = locals_cp['kwargs']
+        del locals_cp['kwargs']
+    else:
+        kwargs_dict = {}
+    return {**locals_cp, **kwargs_dict}
 
 
 class Spec(ivy.Container):
