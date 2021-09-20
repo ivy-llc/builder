51d61163555f02b7d4c9174679e8b7977bad5a35
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-20 18:38:34 +0200

    added unit test for tune running with generic specification for hyperparameter sweep.

--- ivy_builder_tests/test_tune.py
+++ ivy_builder_tests/test_tune.py
@@ -12,15 +12,13 @@ from demos.simple_example import ExampleDataLoader, ExampleNetwork, ExampleTrain
 ray.init()
 
 
-def test_tune_integration(dev_str, call):
-
+def test_tune_numeric_spec(dev_str, call):
     if call is not helpers.torch_call:
         # ToDo: work out why the backend framework is fixed for tune after the first call,
         #  and include other frameworks in test once this is fixed
         pytest.skip()
 
     builder_helpers.remove_dirs()
-    # network_spec_args = {'device': 'cpu:0'}
     trainer_spec_args = {'total_iterations': 10, 'ld_chkpt': False, 'log_freq': 1, 'log_dir': 'log'}
     tuner_spec_args = {'framework': ivy.current_framework_str(),
                        'train_steps_per_tune_step': 2,
@@ -35,6 +33,46 @@ def test_tune_integration(dev_str, call):
                             },
                        'name': 'asynchyperband_test',
                        'num_samples': 5,
+                       'parallel_trials': 1,
+                       'grace_period': 1,
+                       'checkpoint_freq': 0}
+    tuner = builder.build_tuner(ExampleDataLoader, ExampleNetwork, ExampleTrainer,
+                                trainer_spec_args=trainer_spec_args, tuner_spec_args=tuner_spec_args)
+    tuner.tune()
+    builder_helpers.remove_dirs()
+
+
+def test_tune_general_spec(dev_str, call):
+    if call is not helpers.torch_call:
+        # ToDo: work out why the backend framework is fixed for tune after the first call,
+        #  and include other frameworks in test once this is fixed
+        pytest.skip()
+
+    builder_helpers.remove_dirs()
+    trainer_spec_args = {'total_iterations': 10, 'ld_chkpt': False, 'log_freq': 1, 'log_dir': 'log'}
+    tuner_spec_args = {'framework': ivy.current_framework_str(),
+                       'train_steps_per_tune_step': 2,
+                       'network_spec':
+                           {'spec_a':
+                               {
+                                   "configs": [{'param_0': True}, {'param_1': False}],
+                                   "grid": True
+                               },
+                            'spec_b':
+                                {
+                                    "configs": [{'param_0': True}, {'param_0': False}]
+                                },
+                            'spec_c':
+                                {
+                                    "spec_c_a":
+                                        {"configs": [1, 2],
+                                         "grid": True},
+                                    "spec_c_b":
+                                        {"configs": ['100', '200']},
+                                }
+                            },
+                       'name': 'asynchyperband_test',
+                       'num_samples': 1,
                        'parallel_trials': 1,
                        'grace_period': 1,
                        'checkpoint_freq': 0}
