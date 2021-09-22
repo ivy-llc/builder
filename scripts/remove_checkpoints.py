381859c8079bb735d828ee1d14bf4128b0baaa58
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-22 21:17:39 +0200

    extended remove_checkpoints script to operate recursively.

--- scripts/remove_checkpoints.py
+++ scripts/remove_checkpoints.py
@@ -6,9 +6,41 @@ import ivy
 import argparse
 
 
+def prune_checkpoints_in_dir(chkpts_dir, cutoff, last_only):
+    checkpoint_fnames = os.listdir(chkpts_dir)
+    if len(checkpoint_fnames) == 0:
+        print('No checkpoints found in {}'.format(chkpts_dir))
+        return
+    checkpoint_fnames.sort(key=lambda x: int(x.split('-')[-1].split('.')[0]))
+    if last_only:
+        [os.remove(os.path.join(chkpts_dir, cfn)) for cfn in checkpoint_fnames[:-1]]
+        return
+    for checkpoint_fname in checkpoint_fnames:
+        checkpoint_val = int(checkpoint_fname.split('-')[-1].split('.')[0])
+        if ivy.exists(cutoff) and checkpoint_val > cutoff:
+            os.remove(os.path.join(chkpts_dir, checkpoint_fname))
+
+
+def prune_checkpoints(base_dir, cutoff, last_only, is_chkpts=False):
+
+    if is_chkpts:
+        prune_checkpoints_in_dir(base_dir, cutoff, last_only)
+        return
+
+    contents = os.listdir(base_dir)
+    contents.sort()
+
+    for item in contents:
+        if os.path.isdir(item):
+            is_chkpts = False
+            if item == 'chkpts':
+                is_chkpts = True
+            prune_checkpoints(os.path.join(base_dir, item), cutoff, last_only, is_chkpts)
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-cd', '--checkpoint_dir', type=str,
+    parser.add_argument('-bd', '--base_dir', type=str,
                         help='The directory containing the checkpoint files.')
     parser.add_argument('-c', '--cutoff', type=int,
                         help='The cutoff value for the checkpoints, above which existing checkpoints will be removed.')
@@ -17,20 +49,8 @@ def main():
     parsed_args = parser.parse_args()
     if not parsed_args.cutoff and not parsed_args.last_only:
         raise Exception('Either cutoff value must be specified or last_only mode must be set.')
-    checkpoint_dir = ivy.default(parsed_args.checkpoint_dir, os.getcwd())
-    assert checkpoint_dir.split('/')[-1] == 'chkpts'
-    checkpoint_fnames = os.listdir(checkpoint_dir)
-    if len(checkpoint_fnames) == 0:
-        print('No checkpoints found in this directory, exiting.')
-        return
-    checkpoint_fnames.sort(key=lambda x: int(x.split('-')[-1].split('.')[0]))
-    if parsed_args.last_only:
-        [os.remove(os.path.join(checkpoint_dir, cfn)) for cfn in checkpoint_fnames[:-1]]
-        return
-    for checkpoint_fname in checkpoint_fnames:
-        checkpoint_val = int(checkpoint_fname.split('-')[-1].split('.')[0])
-        if ivy.exists(parsed_args.cutoff) and checkpoint_val > parsed_args.cutoff:
-            os.remove(os.path.join(checkpoint_dir, checkpoint_fname))
+    base_dir = ivy.default(parsed_args.base_dir, os.getcwd())
+    prune_checkpoints(base_dir, parsed_args.cutoff, parsed_args.last_only)
 
 
 if __name__ == '__main__':
