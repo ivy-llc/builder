6bad77eb32c2c08ec89573782760340fac8a1dce
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-28 10:50:50 +0200

    updated unit test helpers to wait until tune log is cleared, as sometimes it can take some time for the tune process to join and the logs to clear.

--- ivy_builder_tests/helpers.py
+++ ivy_builder_tests/helpers.py
@@ -1,11 +1,16 @@
 # global
 import os
 import shutil
+import time
 
 
 def remove_dirs(base_dir=None):
     if base_dir is None:
         base_dir = os.getcwd()
+    tune_present = False
+    tune_path = os.path.join(base_dir, 'tune')
+    if os.path.isdir(tune_path):
+        tune_present = True
     shutil.rmtree(os.path.join(base_dir, 'log'), ignore_errors=True)
     shutil.rmtree(os.path.join(base_dir, 'chkpt'), ignore_errors=True)
     shutil.rmtree(os.path.join(base_dir, 'tune'), ignore_errors=True)
@@ -18,3 +23,7 @@ def remove_dirs(base_dir=None):
         shutil.rmtree(serialized_model_path, ignore_errors=True)
     elif os.path.isfile(serialized_model_path):
         os.remove(serialized_model_path)
+    if tune_present:
+        time.sleep(1)
+        if os.path.isdir(tune_path):
+            remove_dirs(base_dir)
