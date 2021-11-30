ef472aa5cd3eda03eb37ae09f7e807623f974f51
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-30 19:21:01 +0100

    version 1.1.7.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ lines = [line for line in lines if not (is_html(line) or is_raw_block(line))]
 long_description = '\n'.join(lines)
 
 setup(name='ivy-builder',
-      version='1.1.6',
+      version='1.1.7',
       author='Ivy Team',
       author_email='ivydl.team@gmail.com',
       description='Build custom Ivy training tasks with clear, hierarchical and robust user specifications.\n',
