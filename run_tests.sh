b8e22bc4a90033c46f8dcd3843fca5ce509776b4
Kareem Morsy <kreem.morsy@hotmail.com>
2023-03-21 09:23:50 +0000

    run_tests.sh is not compatible with docker fix

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker run --rm -it -v "$(pwd)":/ivy_builder unifyai/ivy-builder:latest python3 -m pytest ivy_builder_tests/
+docker run --rm -it -v "$(pwd)":/builder unifyai/ivy-builder:latest python3 -m pytest ivy_builder_tests/
