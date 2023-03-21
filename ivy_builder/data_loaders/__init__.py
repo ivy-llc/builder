1b07cc246341a9ce53dc7bb01351b8939bfa4f37
Kareem Morsy <kreem.morsy@hotmail.com>
2023-03-21 08:33:21 +0000

    Removing specs contamination

--- ivy_builder/data_loaders/__init__.py
+++ ivy_builder/data_loaders/__init__.py
@@ -1,4 +1,16 @@
+import types
+
 from . import specs
 from .specs import *
 from . import seq_data_loader
 from .seq_data_loader import *
+
+__all__ = [
+    name
+    for name, thing in globals().items()
+    if not (
+        name.startswith("_")
+        or name == "specs"
+        or (isinstance(thing, types.ModuleType) and "specs" not in thing.__name__)
+    )
+]
