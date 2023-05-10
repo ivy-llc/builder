69182ceeafddb198d27236efddfb7f5fc013b851
saeedashrraf <48128381+saeedashrraf@users.noreply.github.com>
2023-05-10 16:12:46 +0000

    Reverting commits

--- ivy_builder_tests/helpers.py
+++ ivy_builder_tests/helpers.py
@@ -8,8 +8,7 @@ def remove_dirs(base_dir=None):
     if base_dir is None:
         base_dir = os.getcwd()
     tune_present = False
-    tune_path = os.path.join(base_dir, "ivy_builder_tests")
-    tune_path = os.path.join(tune_path, "/tune")
+    tune_path = os.path.join(base_dir, "tune")
     if os.path.isdir(tune_path):
         tune_present = True
     shutil.rmtree(os.path.join(base_dir, "log"), ignore_errors=True)
