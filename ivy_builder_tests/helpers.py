94ec9c799883b3f2fc7a40b5eab26a214cc27280
Saeed Ashraf <saeedashrraf@gmail.com>
2023-05-11 14:19:28 +0300

    Changes to helpers remove_dirs

--- ivy_builder_tests/helpers.py
+++ ivy_builder_tests/helpers.py
@@ -8,12 +8,11 @@ def remove_dirs(base_dir=None):
     if base_dir is None:
         base_dir = os.getcwd()
     tune_present = False
-    tune_path = os.path.join(base_dir, "tune")
-    if os.path.isdir(tune_path):
-        tune_present = True
+    tune_path = os.path.join(base_dir, "ivy_builder_tests/")
     shutil.rmtree(os.path.join(base_dir, "log"), ignore_errors=True)
     shutil.rmtree(os.path.join(base_dir, "chkpt"), ignore_errors=True)
-    shutil.rmtree(os.path.join(base_dir, "tune"), ignore_errors=True)
+    shutil.rmtree(os.path.join(tune_path, "log"), ignore_errors=True)
+    shutil.rmtree(os.path.join(tune_path, "tune"), ignore_errors=True)
     shutil.rmtree(os.path.join(base_dir, "ray"), ignore_errors=True)
     log_fpath = os.path.join(base_dir, "log.log")
     if os.path.isfile(log_fpath):
@@ -23,6 +22,9 @@ def remove_dirs(base_dir=None):
         shutil.rmtree(serialized_model_path, ignore_errors=True)
     elif os.path.isfile(serialized_model_path):
         os.remove(serialized_model_path)
+    tune_path = os.path.join(base_dir, "tune")
+    if os.path.isdir(tune_path):
+        tune_present = True
     if tune_present:
         time.sleep(2)
         if os.path.isdir(tune_path):
