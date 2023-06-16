213c475f1ae45870aa959ee2916be67971183714
bipinkrishnan <bipinkrishna.p@gmail.com>
2023-06-16 03:55:18 +0000

    Merge pre-build script with installer script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,8 @@
+#!/bin/bash -e
+
+# python3-opencv and git are needed
+apt-get install -y git
+apt-get install -y python3-opencv
+
 pip install -r requirements.txt || exit 1
 pip install -r optional.txt || exit 1
