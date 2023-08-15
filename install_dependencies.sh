25198ea1c078b71c1c98473b06490b73b154b378
Kareem Morsy <kreem.morsy@hotmail.com>
2023-08-15 18:39:44 +0300

    Make `apt-get` work in `root`

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # python3-opencv and git are needed
-apt-get install -y git
-apt-get install -y python3-opencv
+sudo apt-get install -y git || exit 1
+sudo apt-get install -y python3-opencv || exit 1
 
 pip install -r requirements.txt || exit 1
 pip install -r optional.txt || exit 1
